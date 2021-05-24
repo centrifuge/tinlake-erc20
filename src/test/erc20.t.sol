cfd1396b43aa5ee24b279a1b2670c6d9d66644ba
Jeroen Offerijns <jeroen@offerijns.com>
2021-05-24 10:52:14 +0200

    refactor: upgrade to solc 0.6

--- src/test/erc20.t.sol
+++ src/test/erc20.t.sol
@@ -1,22 +1,7 @@
-/// erc20.t.sol
-
+// SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (C) 2015-2019  DappHub, LLC,
 // Copyright (C) 2019 lucasvo
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-pragma solidity >=0.5.12;
+pragma solidity >=0.6.0;
 
 import "ds-test/test.sol";
 
@@ -83,8 +68,8 @@ contract ERC20User {
 
 }
 
-contract Hevm {
-    function warp(uint256) public;
+interface Hevm {
+    function warp(uint256) external;
 }
 
 contract ERC20Test is DSTest {
